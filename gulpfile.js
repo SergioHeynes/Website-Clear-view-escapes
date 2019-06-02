95487a8f2c1563741c6f266e1c0b63b826910b37
Sergio Heynes <sergio.heynes@gmail.com>
2019-06-02 16:44:00 -0500

    Added Gulp to the project. Watcher function ready. Created temp folder. Added postcss features to the project.

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const simpleVars = require('postcss-simple-vars');
+const nested = require('postcss-nested');
+
+
+function taskHTML(done){
+    console.log('Starting HTML task');
+    done();
+}
+
+function taskStyles(done){
+    console.log('Starting styles task');
+    return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([autoprefixer, simpleVars, nested]))
+    .pipe(gulp.dest('./app/temp/styles'));
+    done();
+}
+
+function watch(done){
+    console.log('Starting watch');
+    gulp.watch('./app/index.html', taskHTML);
+    gulp.watch('./app/assets/styles/**/*.css', taskStyles);
+    done();
+}
+
+
+
+exports.taskHTML = taskHTML;
+exports.taskStyles = taskStyles;
+exports.watch = watch;
